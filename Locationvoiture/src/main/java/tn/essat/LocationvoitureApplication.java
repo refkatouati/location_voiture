f268ca5c12525faaf292f726f6b36360b708beae
refka touati <touatirefkaa@gmail.com>
2020-12-22 11:36:07 +0100

    first commit

---
+++ Locationvoiture/src/main/java/tn/essat/LocationvoitureApplication.java
@@ -0,0 +1,13 @@
+package tn.essat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationvoitureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocationvoitureApplication.class, args);
+	}
+
+}
